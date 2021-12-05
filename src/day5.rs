3d71a26c7a85ba34dd848b8ad50d9888b7ec0dae
Matt Kline <matt@bitbashing.io>
2021-12-04 22:08:04 -0800

    Day 5: No need to collect inputs

--- src/day5.rs
+++ src/day5.rs
@@ -60,23 +60,19 @@ fn parse_line(input: &str) -> Line {
 
 #[aoc(day5, part1)]
 pub fn part1(input: &str) -> usize {
-    let hv_lines: Vec<Line> = input
-        .lines()
-        .map(parse_line)
-        .filter_map(|mut l| {
-            if l.is_angled() {
-                None
-            } else {
-                l.sort();
-                Some(l)
-            }
-        })
-        .collect();
+    let hv_lines = input.lines().map(parse_line).filter_map(|mut l| {
+        if l.is_angled() {
+            None
+        } else {
+            l.sort();
+            Some(l)
+        }
+    });
 
     // We could construct a bounding box, then allocate counts for every point,
     // but assume the area is fairly sparse and just use a hashmap of points instead.
     let mut counts: FxHashMap<Point, u16> = FxHashMap::default();
-    for line in &hv_lines {
+    for line in hv_lines {
         if line.is_horizontal() {
             let y = line.start.y;
             for x in line.start.x..=line.end.x {
@@ -96,18 +92,14 @@ pub fn part1(input: &str) -> usize {
 
 #[aoc(day5, part2)]
 pub fn part2(input: &str) -> usize {
-    let lines: Vec<Line> = input
-        .lines()
-        .map(parse_line)
-        .map(|mut l| {
-            l.sort();
-            l
-        })
-        .collect();
+    let lines = input.lines().map(parse_line).map(|mut l| {
+        l.sort();
+        l
+    });
 
     // Ditto
     let mut counts: FxHashMap<Point, u16> = FxHashMap::default();
-    for line in &lines {
+    for line in lines {
         if line.is_vertical() {
             let x = line.start.x;
             for y in line.start.y..=line.end.y {
