327b874f1b4409a357696f8e918ffdd0a1caf0cc
Matt Kline <matt@bitbashing.io>
2021-12-07 01:11:32 -0800

    Day 7: Nah, I was close
    I just didn't realize that you need to truncate the mean value since
    you're dealing with indexes... or something

--- src/day7.rs
+++ src/day7.rs
@@ -1,7 +1,19 @@
 use aoc_runner_derive::aoc;
 
-pub fn crab_posits(input: &str) -> Vec<i16> {
+fn crab_posits(input: &str) -> Vec<i16> {
     input.split(',').map(|n| n.parse().unwrap()).collect()
+}
+
+fn linear_fuel_cost(input: &[i16], to: i64) -> i64 {
+    input.iter().map(|c| (*c as i64 - to).abs()).sum()
+}
+
+fn quadratic_fuel_cost(input: &[i16], to: i64) -> i64 {
+    input.iter().map(|c| {
+        let dist = (*c as i64 - to).abs();
+        // (1..=distance).sum::<i64>()
+        (dist * (dist + 1)) / 2
+    }).sum()
 }
 
 fn median(input: &mut [i16]) -> i16 {
@@ -10,31 +22,17 @@ fn median(input: &mut [i16]) -> i16 {
     *median
 }
 
-fn linear_fuel_cost(input: &[i16], to: i16) -> i64 {
-    input.iter().map(|c| (c - to).abs() as i64).sum()
-}
-
-fn quadratic_fuel_cost(input: &[i16], to: i16) -> i64 {
-    input.iter().map(|c| {
-        let dist = (c - to).abs() as i64;
-        // (1..=distance).sum::<i64>()
-        (dist * (dist + 1)) / 2
-    }).sum()
-}
-
 #[aoc(day7, part1)]
 pub fn part1(input: &str) -> i64 {
     let mut crabs = crab_posits(input);
-    let med = median(&mut crabs);
+    let med = median(&mut crabs) as i64;
     linear_fuel_cost(&crabs, med)
 }
 
 #[aoc(day7, part2)]
 pub fn part2(input: &str) -> i64 {
-    // let crabs = vec![16,1,2,0,4,2,7,1,2,14];
     let crabs = crab_posits(input);
-    let min = crabs.iter().min_by_key(|c| quadratic_fuel_cost(&crabs, **c)).unwrap();
-    // let sum = crabs.iter().fold(0i64, |acc, c| acc + *c as i64);
-    // let mean = (sum as f64 / crabs.len() as f64).round() as i16;
-    quadratic_fuel_cost(&crabs, *min)
+    let sum: i64 = crabs.iter().fold(0i64, |acc, c| acc + *c as i64);
+    let mean = sum / crabs.len() as i64;
+    quadratic_fuel_cost(&crabs, mean)
 }
